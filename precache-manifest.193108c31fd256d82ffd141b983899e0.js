6d7605f925a1829c6396a570a1abe4507f2cafd4
Senik <farsyan.senik@gmail.com>
2020-03-22 16:55:42 +0400

    Updates

---
+++ precache-manifest.193108c31fd256d82ffd141b983899e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f744402c35c93d2e8ae08f726f3d214d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808b0e22717b2f29fe8b",
+    "url": "/static/css/main.f421c46f.chunk.css"
+  },
+  {
+    "revision": "db1b32789f11e286e2d3",
+    "url": "/static/js/2.f5e383e7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f5e383e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808b0e22717b2f29fe8b",
+    "url": "/static/js/main.93041143.chunk.js"
+  },
+  {
+    "revision": "34a59f721360da86a877",
+    "url": "/static/js/runtime-main.bd75f902.js"
+  },
+  {
+    "revision": "1089ebe2c94d39e946b945df4f06fcb1",
+    "url": "/static/media/WPM.1089ebe2.svg"
+  },
+  {
+    "revision": "a21746f5374859d98f7528e1857e3532",
+    "url": "/static/media/loader.a21746f5.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/speed-typing-game/precache-manifest.6783c92d4c10b92ebafeb36312290c66.js"
+  "/precache-manifest.193108c31fd256d82ffd141b983899e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/speed-typing-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
